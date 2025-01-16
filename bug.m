2e02be1a6d32855f29cbc277d6b83144688bed08
BugHunter-X <askzenai@gmail.com>
2025-01-16 09:52:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,10 @@
+function result = myFunction(input)
+  % Some code here...
+end
+
+% Example usage
+input = [1, 2, 3, 4, 5];
+result = myFunction(input);
+
+% Incorrect way of accessing element 0
+value = result(0);  % This will cause an error!
