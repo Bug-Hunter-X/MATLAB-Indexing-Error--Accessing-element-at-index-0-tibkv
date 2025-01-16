959815ad7abf64fbb14e4dfc534ab2d8de3ae2e0
BugHunter-X <askzenai@gmail.com>
2025-01-16 09:52:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,10 @@
+function result = myFunction(input)
+  % Some code here...
+end
+
+% Example usage
+input = [1, 2, 3, 4, 5];
+result = myFunction(input);
+
+% Correct way of accessing the first element
+value = result(1); 
